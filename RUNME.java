fc10517cfdbdd892095d497c7a748429e46c3d4f
ThePaniniMan <prmaringantiacts@gmail.com>
2024-01-03 20:08:08 -0500

    changed gui name to RUNME

--- RUNME.java
+++ RUNME.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class gui{
+public class RUNME{
     public static void main(String[] args){
         theframe MyFrame = new theframe();
     }

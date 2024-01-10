871f6aea84663027395fc97d23c461266a612c0c
Pranav Maringanti <prmaringantiacts@gmail.com>
2024-01-10 14:09:38 -0500

    unstable and unformatted version of ball physics, added frame updater

--- BallFrame.java
+++ BallFrame.java
@@ -1,22 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class BallFrame extends JFrame {
     private BackButton backButton;
     private BouncingBall bouncingBall;
+    public int frameWidth;
+    public int frameHeight;
 
     public BallFrame(Color backgroundColor, JFrame mainFrame) {
         this.getContentPane().setBackground(backgroundColor);
         this.setSize(700, 700);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         this.getContentPane().setLayout(null);
         backButton = new BackButton(this, mainFrame);
         backButton.setBounds(10, 10, 100, 50);
+        backButton.setOpaque(false); // Make backButton transparent
         this.getContentPane().add(backButton);
 
-        bouncingBall = new BouncingBall();
+        bouncingBall = new BouncingBall(this);
         bouncingBall.setBounds(0, 0, 700, 700);
+        bouncingBall.setOpaque(false); // Make bouncingBall transparent
         this.getContentPane().add(bouncingBall);
+
+        // Add a component listener to update the size of the frame whenever it's resized
+        this.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                frameWidth = getWidth();
+                frameHeight = getHeight();
+                System.out.println("Frame width: " + frameWidth + ", Frame height: " + frameHeight);
+            }
+        });
     }
 }
 
@@ -24,8 +41,11 @@ class BouncingBall extends JPanel {
     double x = 0, y = 0, vx = 2, vy = 0;
     double gravity = 0.1, friction = 0.01, restitution = 0.7;
     int radius = 25;
+    int groundHeight; // Height of the ground from the bottom of the frame
+    BallFrame ballFrame;
 
-    public BouncingBall() {
+    public BouncingBall(BallFrame ballFrame) {
+        this.ballFrame = ballFrame;
         Timer timer = new Timer(5, e -> {
             // Apply gravity
             vy += gravity;
@@ -42,17 +62,20 @@ class BouncingBall extends JPanel {
             if (x - radius < 0) {
                 x = radius;
                 vx = -vx * restitution;
-            } else if (x + radius > getWidth()) {
-                x = getWidth() - radius;
+            } else if (x + radius > ballFrame.frameWidth) {
+                x = ballFrame.frameWidth - radius;
                 vx = -vx * restitution;
             }
             if (y - radius < 0) {
                 y = radius;
                 vy = -vy * restitution;
-            } else if (y + radius > getHeight()) {
-                y = getHeight() - radius;
+            } else if (y + radius > ballFrame.frameHeight - groundHeight) {
+                y = ballFrame.frameHeight - groundHeight - radius;
                 vy = -vy * restitution;
             }
+
+            // Update the ground height based on the frame height
+            groundHeight = ballFrame.frameHeight / 5;
 
             repaint();
         });
